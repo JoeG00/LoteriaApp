e7378a7971b04648879c123cc7742b9524e9f445
Joe Garza <heyjoe215@gmail.com>
2020-08-17 23:48:45 -0500

    first commit- card generator function, image renaming from 1-54

---
+++ index.js
@@ -0,0 +1,20 @@
+function Cardgenerator(){
+    const card=[]
+    let currentnum= Math.floor((Math.random() * 54) + 1);
+
+    for (i=0; i<16; i++){
+        let currentnum= Math.floor((Math.random() * 54) + 1);
+        if (card.includes(currentnum)){
+            let newnum= Math.floor((Math.random() * 54) + 1);
+            card.push(newnum)
+        }else{
+        card.push(currentnum)
+        }
+
+    }
+
+    console.log(card)
+
+}
+
+Cardgenerator()
